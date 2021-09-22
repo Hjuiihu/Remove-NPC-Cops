4d83ed3728de068e3e3bdad7106ea0fcda8e875a
Alexis <83204744+Hjuiihu@users.noreply.github.com>
2021-09-22 15:40:35 +0200

    Add files via upload

--- cl_removeAIcops.lua
+++ cl_removeAIcops.lua
@@ -1,3 +1,15 @@
+--[[
+___________________________________________________________________________
+						
+						AUTHOR : Hjuiihu
+
+			Plus aucune armes droppées par les PNJ
+			Peds wont drop weapons no more
+		
+
+____________________________________________________________________________					
+]]
+
 Citizen.CreateThread(function()
     while true do
     Citizen.Wait(0)
@@ -6,3 +18,7 @@ Citizen.CreateThread(function()
     ClearAreaOfCops(playerLocalisation.x, playerLocalisation.y, playerLocalisation.z, 400.0)
     end
 end)
+
+---------------------------------
+---   Copyright by Hjuiihu   ---
+---------------------------------
